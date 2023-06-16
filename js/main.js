58aac8f6133a6c24cb794ac6e26a9055a21c08d5
coby_k <cokalter@gmail.com>
2023-06-16 18:18:35 -0400

    Added css hover effects, refined the button colors, and added js files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const navList = document.querySelector("#navlist");
+
+
+navList.addEventListener("mouseover", function (event) {
+    event.target.style.color("#BEE4F4");
+})
+console.log(navList);
